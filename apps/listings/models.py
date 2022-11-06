5a7b1dced75848a9fdc531232706f67c775df916
justbenjie <filippoknest3@gmail.com>
2022-11-07 00:05:46 +0300

    initial commit

---
+++ apps/listings/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=128)
+    price = models.IntegerField()
+    num_bedrooms = models.IntegerField()
+    num_bathrooms = models.IntegerField()
+    square_footage = models.IntegerField()
+    address = models.CharField(max_length=128)
+    # image
